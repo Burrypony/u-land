6bbf88c7afc73243687f0a1b19406f54263136db
unknown <kiril.chernysh@gmail.com>
2021-04-15 02:41:32 +0300

    added new styles

--- script/script.js
+++ script/script.js
@@ -1,6 +1,13 @@
-$(function() {
-    $('a[href*="#"]').on('click', function(e) {
-      e.preventDefault();
-      $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 500, 'linear');
-    });
+AOS.init();
+
+$(function () {
+  $('a[href*="#"]').on('click', function (e) {
+    e.preventDefault();
+    $('html, body').animate({
+      scrollTop: $($(this).attr('href')).offset().top
+    }, 500, 'linear');
   });
+});
+AOS.init({
+  duration: 1200,
+})

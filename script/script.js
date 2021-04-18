be991bc9109371800b65e768d2471ace7e985363
unknown <kiril.chernysh@gmail.com>
2021-04-19 02:27:27 +0300

    added new things

--- script/script.js
+++ script/script.js
@@ -11,3 +11,32 @@ $(function () {
 AOS.init({
   duration: 1200,
 })
+
+
+function sendMail() {
+  var name = $('#exampleInputEmail1').val()
+  var phone = $('#exampleInputPassword1').val()
+  var messageBody = 'імя: ' + name + '  телефон: ' + phone
+  $.ajax({
+    type: 'POST',
+    url: 'smtp.mailtrap.io',
+    data: {
+      'key': 'fcd8f80888a120358c9048ab0ce41a8b',
+      'message': {
+        'from_email': 'YOUR@EMAIL.HERE',
+        'to': [
+            {
+              'email': 'kiril.chernysh@gmail.com',
+              'name': 'RECIPIENT NAME (OPTIONAL)',
+              'type': 'to'
+            }
+          ],
+        'autotext': 'true',
+        'subject': 'YOUR SUBJECT HERE!',
+        'html': messageBody
+      }
+    }
+   }).done(function(response) {
+     console.log(response); // if you're into that sorta thing
+   });
+}
